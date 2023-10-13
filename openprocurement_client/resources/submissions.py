799f7908fd3d35a7fc1b5d8c5c2fc8738c90cf68
VTinkovan <tinkovan.v@gmail.com>
2023-10-13 07:56:48 +0200

    added patch_submission_document

--- openprocurement_client/resources/submissions.py
+++ openprocurement_client/resources/submissions.py
@@ -1,7 +1,7 @@
 import logging
 
 from openprocurement_client.clients import APIResourceClient
-from openprocurement_client.constants import (SUBMISSIONS)
+from openprocurement_client.constants import (SUBMISSIONS, DOCUMENTS)
 
 LOGGER = logging.getLogger(__name__)
 
@@ -26,5 +26,23 @@ class SubmissionsClient(APIResourceClient):
                                     use_ds_client=use_ds_client,
                                     doc_registration=doc_registration,
                                     access_token=access_token)
+
+    def patch_submission_document(self, submission_id, document_data, document_id,
+                       access_token=None, depth_path=None):
+        return self.patch_resource_item_subitem(
+            submission_id, document_data, DOCUMENTS, document_id,
+            access_token, depth_path
+        )
+
+    def put_submission_document(self, submission_id, subitem_obj, document_id, access_token=None):
+        headers = None
+        if access_token:
+            headers = {'X-Access-Token': access_token}
+        if isinstance(submission_id, dict):
+            headers = {'X-Access-Token': self._get_access_token(submission_id)}
+            resource_item_id = submission_id['data']['id']
+        url = '{}/{}/{}/{}'.format(self.prefix_path, submission_id,
+                                       DOCUMENTS, document_id)
+        return self._create_resource_item(url, subitem_obj, headers=headers, method='PUT')
 
 
