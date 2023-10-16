9d4ed1007a8d3659f461233b655ae17451007fe7
VTinkovan <tinkovan.v@gmail.com>
2023-10-16 16:22:30 +0200

    added post_qualification_document

--- openprocurement_client/resources/qualifications.py
+++ openprocurement_client/resources/qualifications.py
@@ -1,7 +1,7 @@
 import logging
 
 from openprocurement_client.clients import APIResourceClient
-from openprocurement_client.constants import (QUALIFICATIONS)
+from openprocurement_client.constants import (QUALIFICATIONS, DOCUMENTS)
 
 LOGGER = logging.getLogger(__name__)
 
@@ -23,3 +23,26 @@ class QualificationClient(APIResourceClient):
                                     use_ds_client=use_ds_client,
                                     doc_registration=doc_registration,
                                     access_token=access_token)
+
+    def post_qualification_document(self, qualification_id, document_data, access_token=None):
+        return self.create_resource_item_subitem(
+            qualification_id, document_data, DOCUMENTS, access_token=access_token
+        )
+
+    def patch_qualification_document(self, qualification_id, document_data, document_id,
+                       access_token=None, depth_path=None):
+        return self.patch_resource_item_subitem(
+            qualification_id, document_data, DOCUMENTS, document_id,
+            access_token, depth_path
+        )
+
+    def put_qualification_document(self, qualification_id, subitem_obj, document_id, access_token=None):
+        headers = None
+        if access_token:
+            headers = {'X-Access-Token': access_token}
+        if isinstance(qualification_id, dict):
+            headers = {'X-Access-Token': self._get_access_token(qualification_id)}
+            resource_item_id = qualification_id['data']['id']
+        url = '{}/{}/{}/{}'.format(self.prefix_path, qualification_id,
+                                       DOCUMENTS, document_id)
+        return self._create_resource_item(url, subitem_obj, headers=headers, method='PUT')
